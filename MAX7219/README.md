4ea57506a0245a42fb92e9dd621b5ce8cb30e508
Niall Kearney <mantel@gmail.com>
2022-03-19 08:45:03 +0000

    Added max7219.py library, example script and updated readme.

---
+++ MAX7219/README.md
@@ -0,0 +1,5 @@
+MAX7219
+
+Some examples taken from https://www.youtube.com/watch?v=svnf0czdVt8
+
+Learning how to use the MAX7219 display driver with the Pi Pico and Micropython.
---
+++ MAX7219/main.py
@@ -0,0 +1,26 @@
+import max7219
+from machine import Pin, SPI
+from time import sleep
+spi = SPI(0, baudrate=10000000, polarity=1, phase=0, sck=Pin(2), mosi=Pin(3))
+ss = Pin(5, Pin.OUT)
+
+msg = 'void loop Robotech & Automation'
+length = len(msg)
+length = (length*8)
+display = max7219.Matrix8x8(spi, ss, 4)
+display.brightness(1)   # adjust brightness 1 to 15
+display.fill(1)
+display.show()
+sleep(0.5)
+display.fill(0)
+display.show()
+sleep(0.2)
+
+while True:
+    for x in range(32, -length, -1):
+        display.text(msg ,x,0,1)
+        display.show()
+        sleep(0.10)
+        display.fill(0)
+    
+    
