f95c7770832604079458be72113489c23083f8c5
Niall Kearney <mantel@gmail.com>
2023-07-09 08:19:59 +0100

    Added Plasma Stick 2040 examples

--- basic pico/main.py
+++ basic pico/main.py
@@ -1,10 +1,40 @@
-import time
-import machine
+from machine import Pin
+from neopixel import NeoPixel
+from time import sleep
+import random
 
-led = machine.Pin(16, machine.Pin.OUT)
+num_pixels = 16
+colour = (100,0,0)
+
+strip_pin = Pin(15, Pin.OUT)
+pixels = NeoPixel(strip_pin, num_pixels)
+
+def wheel(pos):
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b)
+
+def rainbow_cycle(wait):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels[i] = wheel(pixel_index & 255)
+        pixels.write()
+        sleep(wait)
 
 while True:
-    led.on()
-    time.sleep(1)
-    led.off()
-    time.sleep(1)
+    rainbow_cycle(0.00001)
